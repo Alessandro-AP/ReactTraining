419577b14f13eccae4e34486e7022c1afeb1256f
Alessandro-AP <alessandro.parrinoap@gmail.com>
2022-07-17 14:27:33 +0200

    Create simple frontend with Ant Design

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'antd/dist/antd.min.css'
 import './css/index.css';
 import App from './js/App';
 
--- src/js/App.js
+++ src/js/App.js
@@ -1,14 +1,65 @@
 import '../css/App.css';
 import { Component } from "react";
+import Container from "./Container";
 import { getAllStudents } from "./client";
+import {Table} from 'antd'
 
 class App extends Component {
-  render() {
-    getAllStudents().then( res => res.json().then(students => {
-      console.log(students)
-    }))
-    return <h1>Hello world</h1>
-  }
+
+    state = {
+        students: []
+    }
+
+    componentDidMount() {
+        this.fetchStudents()
+    }
+
+    fetchStudents = () => {
+        getAllStudents()
+            .then(res => res.json()
+                .then(result => {
+                    this.setState({
+                        students : result
+                    })
+                }))
+    }
+
+    render() {
+        const {students} = this.state
+
+        const columns = [
+            {
+                title: 'ID',
+                dataIndex: 'studentId',
+                key: 'studentId',
+            },
+            {
+                title: 'First Name',
+                dataIndex: 'firstName',
+                key: 'firstName',
+            },
+            {
+                title: 'Last Name',
+                dataIndex: 'lastName',
+                key: 'lastName',
+            },
+            {
+                title: 'Gender',
+                dataIndex: 'gender',
+                key: 'gender',
+            },
+            {
+                title: 'Email',
+                dataIndex: 'email',
+                key: 'email',
+            },
+        ];
+
+        if (students && students.length) {
+           return <Container><Table dataSource={students} columns={columns} rowKey='studentId' pagination={false}/></Container>
+        } else
+            return <h1>No Students Found</h1>
+    }
 }
 
 export default App;
